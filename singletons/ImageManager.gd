0e935ae227ffbcc2981a1f2df18cc3a1fb5e15fa
Onur Baykal <theaob@gmail.com>
2024-04-26 15:08:29 +0300

    add imagemanager

---
+++ singletons/ImageManager.gd
@@ -0,0 +1,32 @@
+extends Node
+
+var _item_images: Array = []
+
+func _ready():
+	load_item_images()
+	
+func add_file_to_list(filename: String, path: String) -> void:
+	var full_path = path + "/" + filename
+	
+	var image_data = {
+		"item_name": filename.rstrip(".png"),
+		"item_texture": load(full_path)
+	}
+	
+	_item_images.append(image_data)
+	
+func load_item_images() -> void:
+	var path = "res://assets/glitch"
+	var directory = DirAccess.open(path)
+	
+	if directory == null:
+		print("Image directory not found.")
+		return
+		
+	var files = directory.get_files()
+	
+	for file in files:
+		if ".import" not in file:
+			add_file_to_list(file, path)
+			
+	print("loaded images: ", _item_images.size())
